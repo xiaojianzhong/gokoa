bf8f6fcc5a81c58d190d8f3d5194dbb77542c164
azxj <1158257471@qq.com>
2020-03-02 12:32:56 +0800

    docs: add hello world example

---
+++ examples/hello-world/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/azxj/gokoa"
+)
+
+func main() {
+	app := gokoa.NewApplication(nil)
+
+	app.Use(func(ctx *gokoa.Context, fn func() error) error {
+		ctx.SetBody("hello gokoa")
+		return nil
+	})
+
+	app.Listen(8080)
+}
