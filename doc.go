1f1f7cdbf9fd1022f55610300c2c5765e82786fb
azxj <1158257471@qq.com>
2020-04-10 16:39:12 +0800

    docs: add doc.go

---
+++ doc.go
@@ -0,0 +1,21 @@
+/*
+Package gokoa is a Koa styled web framework written in Go.
+
+	package main
+
+	import (
+		"github.com/azxj/gokoa"
+	)
+
+	func main() {
+		app := gokoa.NewApplication(nil)
+
+		app.Use(func(ctx *gokoa.Context, fn func() error) error {
+			ctx.SetBody("hello gokoa")
+			return nil
+		})
+
+		app.Listen(8080)
+	}
+*/
+package gokoa
